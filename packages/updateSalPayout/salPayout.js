81983c4368a153dee490c111a5258c72c1d3b7dd
Osprey1999 <hiabheek@gmail.com>
2023-10-16 21:47:11 +0530

    chore: added model classes and payout endpoint
    refs: ####
    
    Co-authored-by: Osprey1999 <hiabheek@gmail.com>

--- packages/updateSalPayout/salPayout.js
+++ packages/updateSalPayout/salPayout.js
@@ -1,7 +1,6 @@
 const pgp = require("pg-promise")();
-const salary = require("../../lib/repository/userSalaryComponents");
+const salary = require("../../lib/repository/UserSalary");
 const validateToken = require("../../lib/utils/validateToken");
-
 
 exports.main = async (event, context) => {
   const { headers, payload } = event;
@@ -22,28 +21,48 @@ exports.main = async (event, context) => {
     });
   }
 
-  //to update salary components of a employee for a month and year
+  //to update salary payout for the user --> transaction id, timest
   try {
     const employeeid = tokenDetails.employeeid;
+    const payflag = payload.payflag;
+    const timestamp = new Date();
+    const transactionid = payload.transactionid;
+    const remarks = payload.remarks;
     const month = payload.month;
     const year = payload.year;
-    const label = payload.label;
 
-    const updateSalComponents = await salary.updateEmployeeSalComponents(
-      employeeid,
-      month,
-      year,
-      label
-    );
-    console.log("MESSAGE: DATA UPDATED SUCCESSFULLY");
+    const fetch = await salary.fetch(employeeid, month, year);
 
-    return (res = {
-      statusCode: 200,
-      body: {
-        error: false,
-        message: "Successfully updated the salary details",
-      },
-    });
+    if (fetch.transactionid == null && fetch.payflag == 0) {
+      await salary.updateEmployeeSal(
+        employeeid,
+        payflag,
+        timestamp,
+        transactionid,
+        remarks,
+        month,
+        year
+      );
+      console.log("MESSAGE: DATA UPDATED SUCCESSFULLY");
+
+      return (res = {
+        statusCode: 200,
+        body: {
+          error: false,
+          message: "Successfully updated the salary details",
+        },
+      });
+    } else {
+      console.log("MESSAGE: TRANSACTION DETAILS ALREADY PRESENT " + fetch.id + employeeid);
+
+      return (res = {
+        statusCode: 400,
+        body: {
+          error: true,
+          message: "Salary payout details are already updated",
+        },
+      });
+    }
   } catch (err) {
     console.log("ERROR: " + err);
     return (res = {
